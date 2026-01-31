6b439435ee74c7855f3cf072e1c33f9f6245d33c
shashwath-cyber <shashwath2010official@gmail.com>
2026-01-31 16:41:04 +0530

    Add App.tsx main React component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Electrostatics Explorer</h1>
+    </div>
+  );
+};
+
+export default App;
